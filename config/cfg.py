6c1e7f7ca4d53d978b09f30a5a866dbb154c83ae
root <root@localhost.localdomain>
2021-01-13 18:32:14 +0800

    change yaml

--- config/cfg.py
+++ config/cfg.py
@@ -1,11 +1,13 @@
 import yaml
 
-Config = read_yaml()
-
-def read_yaml():
-      with open('conf.yaml', 'r', encoding='utf-8') as f:
+def read_yaml(file):
+      with open(file, 'r', encoding='utf-8') as f:
             file_content = f.read()
       conf = yaml.load(file_content, yaml.FullLoader)
       print(conf)
       return conf
+
+Config = read_yaml('conf.yaml')
+
+
 
